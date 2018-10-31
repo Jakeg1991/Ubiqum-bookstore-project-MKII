98d6d1337008e65d91ea88d62fcda4360b9bd548
Jake <jakegodwin@rocketmail.com>
2018-10-31 22:24:16 +0100

    stylechanges

--- Vue.js
+++ Vue.js
@@ -1,38 +1,27 @@
- $(document).ready(function () {
-     $('button').on('click', function () {
-         $.fancybox($("a.fancybox"));
-     });
- });
+$('#loader').show(); 
 
-
-
-
- fetch("https://api.myjson.com/bins/zyv02", {
+fetch("https://api.myjson.com/bins/zyv02", {
          method: 'GET',
      })
      .then(function (response) {
          return response.json()
      })
      .then(function (data) {
-         createBookGrid(data),
-             console.log(data)
-     })
+         var bookGrid = new Vue({
+             el: "#bookVue",
+             data: {
+                 books: data.books,
+                 search: "",
+                 loading: true
 
 
- function createBookGrid(data) {
-
-     var bookGrid = new Vue({
-         el: "#bookVue",
-         data: {
-             books: data.books,
-             search: ""
-         },
-         computed: {
-             filteredBooks: function () {
-                 return this.books.filter((book) => {
-                     return book.title.includes(this.search);
-                 })
+             },
+             computed: {
+                 filteredBooks: function () {
+                     return this.books.filter((book) => {
+                         return book.title.includes(this.search);
+                     })
+                 }
              }
-         }
+         })
      })
- }
