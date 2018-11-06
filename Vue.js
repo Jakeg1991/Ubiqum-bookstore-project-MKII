323b182cec63ccfdc250c61e69da7c6049f0761e
Jake <jakegodwin@rocketmail.com>
2018-11-06 14:13:18 +0100

    modal not working

--- Vue.js
+++ Vue.js
@@ -1,27 +1,35 @@
-$('#loader').show(); 
+$('#loader').show();
 
 fetch("https://api.myjson.com/bins/zyv02", {
-         method: 'GET',
-     })
-     .then(function (response) {
-         return response.json()
-     })
-     .then(function (data) {
-         var bookGrid = new Vue({
-             el: "#bookVue",
-             data: {
-                 books: data.books,
-                 search: "",
-                 loading: true
+        method: 'GET',
+    })
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        var bookGrid = new Vue({
+            el: "#bookVue",
+            data: {
+                books: data.books,
+                search: "",
+                loading: true
 
 
-             },
-             computed: {
-                 filteredBooks: function () {
-                     return this.books.filter((book) => {
-                         return book.title.includes(this.search);
-                     })
-                 }
-             }
-         })
-     })
+            },
+            computed: {
+                filteredBooks: function () {
+                    return this.books.filter((book) => {
+                        return book.title.includes(this.search);
+                    })
+                }
+            }
+        })
+    })
+
+function smoothscroll() {
+    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    if (currentScroll > 0) {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - (currentScroll / 5));
+    }
+}
